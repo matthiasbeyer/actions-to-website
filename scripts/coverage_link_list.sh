39a99559ddf35f96872e6d6b9ebadebd3d9a71f6
Matthias Beyer <mail@beyermatthias.de>
2024-04-18 09:41:08 +0200

    Add script to generate coverage links
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ scripts/coverage_link_list.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function bail() {
+    echo "$*" >&2
+    exit 1
+}
+
+main_branch_name="$1"
+website_base_path="$2"
+coverage_subdir="$3"
+
+[[ -z "$main_branch_name" ]] && bail "Missing argument: main branch name (ARG 1)"
+[[ -z "$website_base_path" ]] && bail "Missing argument: website base path (ARG 2)"
+[[ -z "$coverage_subdir" ]] && bail "Missing argument: coverage sub directory (ARG 3)"
+
+git log --format="%H %cs" --first-parent "${main_branch_name}" | \
+    while read -r hash date;
+    do
+        linkroot="${website_base_path}/${coverage_subdir}/${hash}"
+        echo "* [${date} ${hash}](${linkroot}/index.html) ([lcov](${linkroot}/lcov))"
+    done
+
