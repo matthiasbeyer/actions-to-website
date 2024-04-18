b9a66c78da1ffee13306182b0f05a3535366874b
Matthias Beyer <mail@beyermatthias.de>
2024-04-18 12:19:14 +0200

    Add optional parameter to only generate N links
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- scripts/coverage_link_list.sh
+++ scripts/coverage_link_list.sh
@@ -8,10 +8,16 @@ function bail() {
 main_branch_name="$1"
 website_base_path="$2"
 coverage_subdir="$3"
+link_count="$4"
 
 [[ -z "$main_branch_name" ]] && bail "Missing argument: main branch name (ARG 1)"
 [[ -z "$website_base_path" ]] && bail "Missing argument: website base path (ARG 2)"
 [[ -z "$coverage_subdir" ]] && bail "Missing argument: coverage sub directory (ARG 3)"
+
+if [[ -z "$link_count" ]]; then
+    link_count=10
+    echo >&2 "Missing argument: link count (ARG 4), setting to ${link_count}"
+fi
 
 echo "<table>"
 echo "<tr>"
@@ -20,7 +26,7 @@ echo "  <th>HTML Report</th>"
 echo "  <th>lcov</th>"
 echo "</tr>"
 
-git log --format="%h %H %cs" --first-parent "${main_branch_name}" | \
+git log -n "${link_count}" --format="%h %H %cs" --first-parent "${main_branch_name}" | \
     while read -r shorthash hash date;
     do
         linkroot="${website_base_path}/${coverage_subdir}/${hash}"
