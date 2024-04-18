c90f3b13489cf77ef848046cae7e196cb9a5bd2e
Matthias Beyer <mail@beyermatthias.de>
2024-04-18 10:19:02 +0200

    Add script to update the coverage list
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- scripts/default.nix
+++ scripts/default.nix
@@ -9,11 +9,22 @@ let
 
     text = builtins.readFile ./coverage_link_list.sh;
   };
+
+  # We cannot use this inside the flake, because we do not have the git
+  # repository available, but we can at least build the script as application,
+  # so we get all the automatic shell script checking
+  updateCoverageList = pkgs.writeShellApplication {
+    name = "updateCoverageList";
+    runtimeInputs = [ coverageLinkList ];
+
+    text = builtins.readFile ./update_coverage_list.sh;
+  };
 in
 {
   packages = {
     inherit
       coverageLinkList
+      updateCoverageList
       ;
   };
 }
