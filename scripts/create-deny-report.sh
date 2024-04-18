bfb0338ce6ef2efaf3f9399bf7f752f67fd74707
Matthias Beyer <mail@beyermatthias.de>
2024-04-18 15:51:55 +0200

    debug SHITHUB ACTIONS
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- scripts/create-deny-report.sh
+++ scripts/create-deny-report.sh
@@ -3,4 +3,6 @@
 gitrev="$(git rev-parse HEAD)"
 shortrev="$(echo "$gitrev" | head -c 10)"
 
-cargo deny --format json check all |& tee file | jq -n "{\"data\":[inputs],\"rev\":\"${gitrev}\",\"shortrev\":\"${shortrev}\"}" || cat file
+cargo deny --format json check all > file 2>&1
+cat file >&2
+cat file | jq -n "{\"data\":[inputs],\"rev\":\"${gitrev}\",\"shortrev\":\"${shortrev}\"}"
