ba6430f72b187fa17155375c3e4b757f2a15c0a8
Matthias Beyer <mail@beyermatthias.de>
2024-04-19 13:32:18 +0200

    Revert "debug SHITHUB ACTIONS"
    This reverts commit bfb0338ce6ef2efaf3f9399bf7f752f67fd74707.

--- scripts/create-deny-report.sh
+++ scripts/create-deny-report.sh
@@ -3,6 +3,4 @@
 gitrev="$(git rev-parse HEAD)"
 shortrev="$(echo "$gitrev" | head -c 10)"
 
-cargo deny --format json check all > file 2>&1
-cat file >&2
-cat file | jq -n "{\"data\":[inputs],\"rev\":\"${gitrev}\",\"shortrev\":\"${shortrev}\"}"
+cargo deny --format json check all |& tee file | jq -n "{\"data\":[inputs],\"rev\":\"${gitrev}\",\"shortrev\":\"${shortrev}\"}" || cat file
