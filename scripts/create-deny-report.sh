eeef32e5b2b8566198a00daadc489a65929a454d
Matthias Beyer <mail@beyermatthias.de>
2024-04-19 13:32:54 +0200

    Ensure cargo-deny does not exit with error if there are deny checks failing
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- scripts/create-deny-report.sh
+++ scripts/create-deny-report.sh
@@ -3,4 +3,6 @@
 gitrev="$(git rev-parse HEAD)"
 shortrev="$(echo "$gitrev" | head -c 10)"
 
-cargo deny --format json check all |& jq -n "{\"data\":[inputs],\"rev\":\"${gitrev}\",\"shortrev\":\"${shortrev}\"}"
+{
+    cargo deny --format json check all ||:
+} |& jq -n "{\"data\":[inputs],\"rev\":\"${gitrev}\",\"shortrev\":\"${shortrev}\"}"
