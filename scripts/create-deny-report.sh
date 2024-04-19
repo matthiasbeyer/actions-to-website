9739866a5ff6ce2cdd03504066c7a4a6fc5be19d
Matthias Beyer <mail@beyermatthias.de>
2024-04-19 13:35:30 +0200

    Print cargo-deny output to stderr
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- scripts/create-deny-report.sh
+++ scripts/create-deny-report.sh
@@ -5,4 +5,8 @@ shortrev="$(echo "$gitrev" | head -c 10)"
 
 {
     cargo deny --format json check all ||:
-} |& jq -n "{\"data\":[inputs],\"rev\":\"${gitrev}\",\"shortrev\":\"${shortrev}\"}"
+} &> tmpfile
+
+cat tmpfile >&2
+
+jq -n "{\"data\":[inputs],\"rev\":\"${gitrev}\",\"shortrev\":\"${shortrev}\"}" < tmpfile
