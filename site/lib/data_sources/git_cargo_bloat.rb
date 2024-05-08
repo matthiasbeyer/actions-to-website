2e07ae00ca1b011e772c9b8c4fb5ae38737cd3c2
Matthias Beyer <mail@beyermatthias.de>
2024-05-08 16:13:02 +0200

    WIP: Generate bloat graph
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- site/lib/data_sources/git_cargo_bloat.rb
+++ site/lib/data_sources/git_cargo_bloat.rb
@@ -15,3 +15,29 @@ class GitCargoBloatDataSource < GitDataSource
       end
   end
 end
+
+class GitCargoBloatGraphDataSource < GitCargoBloatDataSource
+  require 'gruff'
+  require 'json'
+
+  identifier :gitbloatgraph
+
+  def items
+    g = Gruff::Line.new
+    g.title = "cargo-bloat file size"
+
+    self.walker_for_first_parent
+      .each
+      .to_a
+      .map { |c| c.notes("refs/notes/cargo-bloat-report") }
+      .reject(&:nil?)
+      .map do |note|
+        item = JSON.load(note[:message])
+        g.data item["gitrev"], item["data"]["file-size"]
+      end
+
+    [
+      new_item(g.to_image.to_blob, {}, "/cargobloat.png")
+    ]
+  end
+end
