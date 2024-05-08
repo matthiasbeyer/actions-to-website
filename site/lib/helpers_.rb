eb216efd324fba8c218d807a4b354c4720862d1a
Matthias Beyer <mail@beyermatthias.de>
2024-05-08 13:12:08 +0200

    Rewrite helpers to get path with base url
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- site/lib/helpers_.rb
+++ site/lib/helpers_.rb
@@ -6,8 +6,25 @@ use_helper GithubHelpers
 use_helper DataHelpers
 
 module Utils
+
+  def default_env?
+    @config.env_name.nil? || @config.env_name == "default"
+  end
+
+  def get_env
+    if default_env?
+      @config
+    else
+      @config[:environments][@config.env_name.to_sym]
+    end
+  end
+
+  def env_base_url
+    get_env[:base_url] || ""
+  end
+
   def with_base_url path
-    @config[:base_url] + path
+    env_base_url + path
   end
 end
 use_helper Utils
